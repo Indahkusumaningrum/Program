729fca06bd23c64b59f63d6c96b381c89f7ecef5
Indah Kusuma Ningrum <112399826+Indahkusumaningrum@users.noreply.github.com>
2023-01-31 22:34:26 +0700

    Update searching_array.cpp

--- searching_array.cpp
+++ searching_array.cpp
@@ -2,16 +2,14 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int angka[] = {4, 7, 9, 11, 4, 2, 5, 2, 8, 10};
+int angka[] = {4, 7, 9, 11, 4, 2, 5, 2, 8, 10};
 	int panjang = sizeof(angka)/sizeof(*angka);
 	int cari, hasil, banyak_data;
 	hasil = 0;
 	char ulang;
-	
 
-do{
-	system("CLS");
+void program (){
+
 	cout << "--------------------\n";
 	cout << "Data tersedia : \n";
 	cout << "--------------------\n";	
@@ -30,31 +28,35 @@ for (int i = 0; i < panjang; i++){
 	}
 }
 
-
 	if (hasil == 0){
 	cout << "DATA TIDAK DITEMUKAN" << endl;
 	}else{
-	cout << "Data " << cari << " ditemukan di : ";
-	for (int i = 0; i < panjang; i++){
-		if(cari == angka[i]){
+		cout << "Data " << cari << " ditemukan di : ";
+		for (int i = 0; i < panjang; i++){
+			if(cari == angka[i]){
 			cout << "\nIndeks ke-" << i << endl;
+			}
 		}
 	}
+	
 }
+int main(){
 
-
+	program();
+	
+	
 cout << "\n\nApakah anda ingin melakukan searching lagi? (y/n)\n";
 cin >> ulang;
 
 system("CLS");
-
-	if(ulang == 'n' || ulang == 'N'){
+	
+	if (ulang == 'y' || ulang == 'Y'){
+		program();
+	}else if(ulang == 'n' || ulang == 'N'){
 		cout << "\t\tTERIMA KASIH TELAH MENGGUNAKAN PROGRAM INI";		
 	} else {
 		cout << "INPUTAN ANDA TIDAK SESUAI!!!";
 	}
-	
-} while (ulang == 'y' || ulang == 'Y');
 
 return 0;
 }
