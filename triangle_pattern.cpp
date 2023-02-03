f3dd78f556f414393b32b24bc3054dc11e0c90fd
Indah Kusuma Ningrum <112399826+Indahkusumaningrum@users.noreply.github.com>
2023-02-03 10:20:44 +0700

    Add files via upload

---
+++ triangle_pattern.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	
+	cout << "Masukkan panjang pola : ";
+	cin >> n;
+	
+	cout << "---------------\n";
+	cout << "POLA 1\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = 1; j <=i; j++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	cout << "---------------\n";
+	cout << "POLA 2\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = n; j >=i; j--){
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	
+	cout << "---------------\n";
+	cout << "POLA 3\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = 1; j < i; j++){
+			cout << " ";
+		}
+		
+		for(int k = n; k >= i; k--){
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	cout << "---------------\n";
+	cout << "POLA 4\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = n; j > i; j--){
+			cout << " ";
+		}
+		
+		for(int k = 1; k <= i; k++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	cout << "---------------\n";
+	cout << "POLA 5\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = n; j > i; j--){
+			cout << " ";
+		}
+		
+		for(int k = 1; k <= i; k++){
+			cout << "*";
+		}
+		
+		for(int l = 1; l < i; l++){
+			cout << "*";
+		}		
+		cout << endl;
+	}
+	
+	cin.get();
+	return 0;
+}
