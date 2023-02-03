1d6b4bb2be580e95aec3d9e4991e407ac65a31d4
Indah Kusuma Ningrum <112399826+Indahkusumaningrum@users.noreply.github.com>
2023-02-03 12:47:44 +0700

    Update triangle_pattern.cpp

--- triangle_pattern.cpp
+++ triangle_pattern.cpp
@@ -85,6 +85,62 @@ int main(){
 		cout << endl;
 	}
 	
+	cout << "---------------\n";
+	cout << "POLA 6\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = 1; j < i; j++){
+			cout << " ";
+		}
+		
+		for(int k = n; k >= i; k--){
+			cout << "*";
+		}
+		
+		for(int l = n; l > i; l--){
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	
+	cout << "---------------\n";
+	cout << "POLA 7\n";
+	cout << "---------------\n";
+
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = n; j > i; j--){
+			cout << " ";
+		}
+		
+		for(int k = 1; k <= i; k++){
+			cout << "*";
+		}
+		
+		for(int l = 1; l < i; l++){
+			cout << "*";
+		}		
+		cout << endl;
+	}
+	for (int i = 1; i <= n; i++){
+		
+		for(int j = 1; j < i; j++){
+			cout << " ";
+		}
+		
+		for(int k = n; k >= i; k--){
+			cout << "*";
+		}
+		
+		for(int l = n; l > i; l--){
+			cout << "*";
+		}
+		cout << endl;
+	}	
+	
 	cin.get();
 	return 0;
 }
