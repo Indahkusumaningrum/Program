5b2e94d37d97e7950e8e1aa5817bde971de25148
Indah Kusuma Ningrum <112399826+Indahkusumaningrum@users.noreply.github.com>
2023-02-04 16:53:07 +0700

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+//Indah Kusuma Ningrum [04/02/2023, 16.45]
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+
+	cout << "Masukkan berapa banyak hello world yang diinginkan : ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++){
+		cout << "Hello World!\n";
+	}
+
+cin.get();
+return 0;
+}
