adfc834c385e8536df21638031f889abbb35610d
Indah Kusuma Ningrum <112399826+Indahkusumaningrum@users.noreply.github.com>
2023-02-16 19:26:17 +0700

    Create continue_break.cpp

---
+++ continue_break.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  int n;
+  char pilihan;
+  
+  cout << "Masukkan banyaknya angka (n) : ";
+  cin >> n;
+  
+  cout << "\n\nPILIHAN : "\n;
+  cout << "[1]. SKIP ANGKA TERTENTU\n";
+  cout << "[2]. BERHENTI DI ANGKA TERTENTU;
+  cin >> pilihan;
+  
+  if(pilihan == '1'){
+    
+  } else if (pilihan == '2'){
+    
+  } else {
+      cout << "INPUTAN LU SALAH CUYY!!!";
+  
+  return 0;
+}
